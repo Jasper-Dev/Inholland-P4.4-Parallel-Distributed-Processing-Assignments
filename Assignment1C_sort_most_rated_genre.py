e322fbec680f8b3d5d640fbdf5923684978f0829
Jasper-Dev <jasperstedema@hotmail.com>
2022-06-25 07:30:16 +0200

    v0.5 modified: Assignment1C_sort_most_rated_genre.py

--- Assignment1C_sort_most_rated_genre.py
+++ Assignment1C_sort_most_rated_genre.py
@@ -36,13 +36,13 @@ class Assignment1C_sort_most_rated_genre(MRJob):
 
         if len(split_by_tab := line.split(TAB_DELIMITER)) == 4:   
                 (userID, movieID, rating, timestamp) = split_by_tab
-                yield movieID, (split_by_tab, 1)
+                yield "rating", (split_by_tab, 1)
 
         elif len(split_by_pipe := line.split(PIPE_DELIMITER)) == 24:
                 (movieID, movie_title, _, _, _, unknown, action, adventure, animation, children, comedy, crime,
                 documentary, drama, fantasy, film_noir, horror, musical, mystery, romance, 
                 scifi, thriller, war, western) = split_by_pipe
-                yield movieID, (split_by_pipe, 1)
+                yield "metadata", (split_by_pipe, 1)
 
         else:
             # if len(split_by_tab) != 1 & len(split_by_pipe) != 1:
